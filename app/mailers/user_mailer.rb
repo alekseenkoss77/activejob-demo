f0948d95af80fb9acadb32da92d5b2c6a99224be
Sergei Alekseenko <alekseenkoss@gmail.com>
2020-04-27 11:22:02 +0300

    test mailer

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,9 @@
+class UserMailer < ApplicationMailer
+  def welcome(email, name)
+    return if email.blank?
+
+    @name = name
+
+    mail to: email, subject: I18n.t('emails.subjects.welcome')
+  end
+end
